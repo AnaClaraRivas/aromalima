24281907fb400a5f0edf2ca76f502942a9bb3516
AnaClaraRivas <anaclararivasbronzeri@gmail.com>
2026-05-02 11:00:46 -0300

    imagens corrigido

--- src/paginas/Inicial.jsx
+++ src/paginas/Inicial.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import "../styles/global.css";
 import "../styles/inicial.css";
 
-
 import CardBebida from "../componentes/CardBebida";
+
+import cafeImg from "../assets/cafeinicial.png";
+import caramelo from "../assets/caramelogelado.png";
+import latte from "../assets/lattedourado.png";
+import choco from "../assets/chocolima.png";
 
 export default function Inicial() {
   return (
@@ -13,18 +17,21 @@ export default function Inicial() {
           <p className="sub">Desde 1984</p>
           <h1>AROMA LIMA</h1>
           <p>Mais que café, uma experiência.</p>
+
           <button className="botao-padrao">
             Confira nossos cafés
           </button>
         </div>
-        <img src="/src/assets/cafeinicial.png" />
+
+        {/* ✅ usa a imagem importada */}
+        <img src={cafeImg} alt="Café" />
       </section>
 
       {/* BEBIDAS */}
       <section className="bebidas">
-        <CardBebida nome="Caramelo Gelado" imagem="/src/assets/caramelogelado.png" />
-        <CardBebida nome="Latte Dourado" imagem="/src/assets/lattedourado.png" />
-        <CardBebida nome="Choco Lima" imagem="/src/assets/chocolima.png" />
+        <CardBebida nome="Caramelo Gelado" imagem={caramelo} />
+        <CardBebida nome="Latte Dourado" imagem={latte} />
+        <CardBebida nome="Choco Lima" imagem={choco} />
       </section>
     </div>
   );

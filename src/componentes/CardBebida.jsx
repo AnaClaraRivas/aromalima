1fb8ea08660c6ef75efef0b115b748285df72843
AnaClaraRivas <anaclararivasbronzeri@gmail.com>
2026-05-03 18:16:57 -0300

    inicial

--- src/componentes/CardBebida.jsx
+++ src/componentes/CardBebida.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { FaStar } from "react-icons/fa";
 
-export default function CardBebida({ nome, imagem }) {
+export default function CardBebida({ nome, imagem, nota = "5.0" }) {
   return (
     <div className="card-bebida">
-      <img src={imagem} alt={nome} />
-      <p>{nome}</p>
+      <div className="img-container">
+        <img src={imagem} alt={nome} />
+
+        <div className="badge">
+          <span className="nota">{nota}</span>
+
+          <FaStar className="estrela" />
+        </div>
+      </div>
+
+      <p className="nome-bebida">{nome}</p>
     </div>
   );
 }
--- src/paginas/Inicial.jsx
+++ src/paginas/Inicial.jsx
@@ -10,16 +10,19 @@ import cafeImg from "../assets/cafeinicial.png";
 import caramelo from "../assets/caramelogelado.png";
 import latte from "../assets/lattedourado.png";
 import choco from "../assets/chocolima.png";
+import preparo from "../assets/preparo.png";
+import barista from "../assets/barista.png";
+
 
 export default function Inicial() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <section className="capa">
         <div>
           <p className="sub">Desde 1984</p>
           <h1>AROMA LIMA</h1>
-          <p>Mais que café, uma experiência.</p>
+          <p>Mais que café,uma experiência.</p>
 
           <button className="botao-padrao">
             Confira nossos cafés
@@ -33,7 +36,35 @@ export default function Inicial() {
         <CardBebida nome="Caramelo Gelado" imagem={caramelo} />
         <CardBebida nome="Latte Dourado" imagem={latte} />
         <CardBebida nome="Choco Lima" imagem={choco} />
+
+        <p className="texto-final">
+          Deseja experimentar um café especial? Acesse agora.
+        </p>
       </section>
+
+      {/* WORKSHOPS */}
+      <section className="workshops">
+        <div className="workshop-conteudo">
+          <div className="imgwork">
+            <img src={barista} alt="Mão derramando café" className="imgworkprincipal" />
+          </div>
+
+          <div className="workshop-info">
+            <div className="workshop-texto">
+              <h2>Aprenda a arte do café</h2>
+              <p>Participe dos nossos workshops e</p> 
+              <p>descubra técnicas de preparo,</p>
+              <p>moagem e extração.</p>
+            </div>
+
+            <a href="#" className="link-cursos">
+              Confira mais cursos e workshops ↗
+            </a>
+          </div>
+
+        </div>
+      </section>
+
     </div>
   );
 }
